7d007e912773ab68ada47e84c807207372c4c37d
numcayX2 <sokulshop5@gmail.com>
2026-01-08 13:42:50 +0700

    Alpha version

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+// src/main.ts
+
+import { ValidationPipe } from '@nestjs/common';
+
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // เปิดใช้ ValidationPipe
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // ตัด field แปลกปลอมทิ้งอัตโนมัติ
+
+      forbidNonWhitelisted: true, // (Optional) แจ้ง Error ถ้ามี field แปลกปลอม
+    }),
+  );
+
+  await app.listen(3000);
+}
+
+bootstrap();
---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,24 @@
+// src/products/dto/create-product.dto.ts
+
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  Min,
+  IsOptional,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
+  price: number;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+}
